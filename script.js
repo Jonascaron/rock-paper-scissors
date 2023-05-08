20c10bf542df04675fe4bb35b78d7e14b274bb64
Jonascaron <jonas.caron16@gmail.com>
2023-05-08 16:14:31 +0200

    changes made

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
-var gamecount = 0;
-var playerScore = 0;
-var computerScore = 0;
+let gamecount = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 console.log('game of 5');
 
-function getComputerChoice() {
-    var answers = [ "Rock", "Paper", "Scissors"];
+getComputerChoice = () => {
+    const answers = [ "Rock", "Paper", "Scissors"];
     return answers[Math.floor(Math.random() * answers.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        return 'It`s a tie! you both picked ' + playerSelection;
-
-    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
-        playerScore++;
-        return "You win! Rock beats Scissors";
-
-    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
-        playerScore++;
-        return "You win! Paper beats Rock";
-
-    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
-        playerScore++;
-        return "You win! Scissors beats Paper";
-
-    } else {
-        computerScore++;
-        return 'You lose! '+ computerSelection + ' beats ' + playerSelection;
+playRound = (playerSelection, computerSelection) => {
+    switch (true) {
+        case playerSelection.toLowerCase() == computerSelection.toLowerCase():
+            return 'It`s a tie! you both picked ' + playerSelection;
+            break;
+        case playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors":
+            return "You win! Rock beats Scissors";
+            break;
+        case playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock":
+            return "You win! Paper beats Rock";
+            break;
+        case playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "Paper":
+            return "You win! Scissors beats Paper";
+            break;
+    
+        default:
+            return 'You lose! '+ computerSelection + ' beats ' + playerSelection;
+            break;
     }
 }
 
-game();
-
-function game() {
+game = () => {
     gamecount++;
-    var playerchoice = prompt('choose rock, paper or scissors')
-    var computerchoice = getComputerChoice();
-    console.log(gamecount);
+    let playerchoice = prompt('choose rock, paper or scissors')
+    let computerchoice = getComputerChoice();
+    console.log(gamecount.toString());
     console.log('player choice is ' + playerchoice);
     console.log('computer choice is ' + computerchoice);
     console.log(playRound(playerchoice, computerchoice));
@@ -47,7 +44,7 @@ function game() {
             console.log('computer wins the game of 5');
 
         } else if (playerScore == computerScore) {
-            console.log('it`s an tie');
+            console.log('the game of 5 is an tie');
 
         } else {
             console.log('You win the game of 5');
@@ -56,3 +53,5 @@ function game() {
         game();
     }
 }
+
+game();
